dbe3607f5f2c72826f2fac7ca31fae010f91cdb5
Aandreba <aandreba@gmail.com>
2022-02-09 23:53:26 +0100

    unit test for sum fixed

--- tests/main.rs
+++ tests/main.rs
@@ -6,54 +6,148 @@ use rand::random;
 macro_rules! test_other {
     ($($fun:ident $(as $name:ident)?),+) => {
         $(
-            #[test]
-            pub fn $fun () {
-                test_other!($fun, $($name,)?
-                    [f32;2] as f32x2,
-                    [f32;4] as f32x4,
-                    [f32;6] as f32x6,
-                    [f32;8] as f32x8,
-                    [f32;10] as f32x10,
-                    [f32;12] as f32x12,
-                    [f32;14] as f32x14,
-                    [f32;16] as f32x16,
-                
-                    [f64;2] as f64x2,
-                    [f64;4] as f64x4,
-                    [f64;6] as f64x6,
-                    [f64;8] as f64x8,
-                    [f64;10] as f64x10,
-                    [f64;12] as f64x12,
-                    [f64;14] as f64x14,
-                    [f64;16] as f64x16
-                );
-            }
+            test_other!($fun, $($name,)?
+                [f32;2] as f32x2,
+                [f32;4] as f32x4,
+                [f32;6] as f32x6,
+                [f32;8] as f32x8,
+                [f32;10] as f32x10,
+                [f32;12] as f32x12,
+                [f32;14] as f32x14,
+                [f32;16] as f32x16,
+            
+                [f64;2] as f64x2,
+                [f64;4] as f64x4,
+                [f64;6] as f64x6,
+                [f64;8] as f64x8,
+                [f64;10] as f64x10,
+                [f64;12] as f64x12,
+                [f64;14] as f64x14,
+                [f64;16] as f64x16
+            );
         )*
     };
 
     ($fun:ident, $name:ident, $([$ty:ident;$len:literal] as $target:ident),+) => {
-        $(
-            let alpha : [$ty;$len] = random();
-            let beta : [$ty;$len] = random();
-            let naive = alpha.into_iter()
-                .zip(beta.into_iter())
-                .map(|(x, y)| x.$fun(y));
-
-            let alpha = <$target>::new(alpha);
-            let beta = <$target>::new(beta);
-            let simd = alpha.$name(beta);
-
-            Into::<[$ty;$len]>::into(simd)
-                .into_iter()
-                .zip(naive)
-                .for_each(|(simd, naive)| assert_eq!(simd, naive, concat!("'", stringify!($fun), "' failed for '", stringify!($target), "'")));
-        )*
+        #[test]
+        pub fn $name() {
+            $(
+                let alpha : [$ty;$len] = random();
+                let beta : [$ty;$len] = random();
+                let naive = alpha.into_iter()
+                    .zip(beta.into_iter())
+                    .map(|(x, y)| x.$fun(y));
+    
+                let alpha = <$target>::new(alpha);
+                let beta = <$target>::new(beta);
+                let simd = alpha.$name(beta);
+    
+                Into::<[$ty;$len]>::into(simd)
+                    .into_iter()
+                    .zip(naive)
+                    .for_each(|(simd, naive)| assert!((simd - naive).abs() <= $ty::EPSILON, stringify!($fun)));
+            )*
+        }
     };
 
     ($fun:ident, $([$ty:ident;$len:literal] as $target:ident),+) => {
+        test_other!($fun, $fun $(,[$ty;$len] as $target)*);
+    };
+}
+
+macro_rules! test_mappings {
+    ($($fun:ident $(as $name:ident)?),+) => {
         $(
-            test_other!($fun, $fun, [$ty;$len] as $target);
+            test_mappings!($fun, $($name,)?
+                [f32;2] as f32x2,
+                [f32;4] as f32x4,
+                [f32;6] as f32x6,
+                [f32;8] as f32x8,
+                [f32;10] as f32x10,
+                [f32;12] as f32x12,
+                [f32;14] as f32x14,
+                [f32;16] as f32x16,
+            
+                [f64;2] as f64x2,
+                [f64;4] as f64x4,
+                [f64;6] as f64x6,
+                [f64;8] as f64x8,
+                [f64;10] as f64x10,
+                [f64;12] as f64x12,
+                [f64;14] as f64x14,
+                [f64;16] as f64x16
+            );
         )*
+    };
+
+    ($fun:ident, $name:ident, $([$ty:ident;$len:literal] as $target:ident),+) => {
+        #[test]
+        pub fn $name() {
+            $(
+                let alpha : [$ty;$len] = random();
+                let naive = alpha.into_iter()
+                    .map(|x| x.$fun());
+    
+                let alpha = <$target>::new(alpha);
+                let simd = alpha.$name();
+    
+                Into::<[$ty;$len]>::into(simd)
+                    .into_iter()
+                    .zip(naive)
+                    .for_each(|(simd, naive)| assert!((simd - naive).abs() <= $ty::EPSILON, stringify!($target)));
+            )*
+        }
+    };
+
+    ($fun:ident, $([$ty:ident;$len:literal] as $target:ident),+) => {
+        test_mappings!($fun, $fun $(,[$ty;$len] as $target)*);
+    };
+}
+
+macro_rules! test_horiz {
+    ($($fun:ident $(as $name:ident)?),+) => {
+        $(
+            test_horiz!($fun, $($name,)?
+                [f32;2] as f32x2,
+                [f32;4] as f32x4,
+                [f32;6] as f32x6,
+                [f32;8] as f32x8,
+                [f32;10] as f32x10,
+                [f32;12] as f32x12,
+                [f32;14] as f32x14,
+                [f32;16] as f32x16,
+            
+                [f64;2] as f64x2,
+                [f64;4] as f64x4,
+                [f64;6] as f64x6,
+                [f64;8] as f64x8,
+                [f64;10] as f64x10,
+                [f64;12] as f64x12,
+                [f64;14] as f64x14,
+                [f64;16] as f64x16
+            );
+        )*
+    };
+
+    ($fun:ident, $name:ident, $([$ty:ident;$len:literal] as $target:ident),+) => {
+        #[test]
+        pub fn $name () {
+            $(
+                let alpha : [$ty;$len] = random();
+                let naive = alpha.into_iter()
+                    .reduce(|x, y| x.$fun(y)).unwrap();
+
+                let alpha = <$target>::new(alpha);
+                let simd = alpha.$name();
+
+                let diff = (naive - simd).abs();
+                assert!(diff <= $ty::EPSILON * ($len as $ty), concat!("Comparison filed for '", stringify!($target), "'"));
+            )*
+        }
+    };
+
+    ($fun:ident, $([$ty:ident;$len:literal] as $target:ident),+) => {
+        test_horiz!($fun, $fun $(,[$ty;$len] as $target)*);
     };
 }
 
@@ -73,6 +167,14 @@ macro_rules! test_index {
 test_other!(
     add, sub, mul, div, 
     min as vmin, max as vmax
+);
+
+test_horiz!(
+    min, max, add as sum
+);
+
+test_mappings!(
+    neg, abs, sqrt
 );
 
 #[test]
