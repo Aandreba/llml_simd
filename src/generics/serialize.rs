7d45a2a19457a27bd8cd0d834eec026a7aaceaaa
Aandreba <aandreba@gmail.com>
2022-02-10 01:04:03 +0100

    unit testing for serde and random

--- src/generics/serialize.rs
+++ src/generics/serialize.rs
@@ -22,29 +22,43 @@ macro_rules! impl_ser {
 }
 
 macro_rules! impl_de {
-    () => {
-        
+    ($([$ty:ident;$len:literal] as $target:ident),+) => {
+        $(
+            impl<'de> Deserialize<'de> for $target {
+                fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: serde::Deserializer<'de> {
+                    let array = <[$ty;$len] as Deserialize<'de>>::deserialize(deserializer)?;
+                    Ok(Self::from(array))
+                }
+            }
+        )*
     };
 }
 
 macro_rules! impl_serde {
-    () => {
-        
+    ($([$ty:ident;$len:literal] as $target:ident),+) => {
+        $(
+            impl_ser!([$ty;$len] as $target);
+            impl_de!([$ty;$len] as $target);
+        )*
     };
 }
 
-impl<'de> Deserialize for f32x2 {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: serde::Deserializer<'de> {
-        struct LocalVisitor;
+impl_serde!(
+    [f32;2] as f32x2,
+    [f32;4] as f32x4,
+    [f32;6] as f32x6,
+    [f32;8] as f32x8,
+    [f32;10] as f32x10,
+    [f32;12] as f32x12,
+    [f32;14] as f32x14,
+    [f32;16] as f32x16,
 
-        impl<'a> Visitor<'a> for LocalVisitor {
-            type Value = f32x2;
-
-            fn expecting(&self, formatter: &mut alloc::fmt::Formatter) -> alloc::fmt::Result {
-                todo!()
-            }
-        }
-
-        deserializer.deserialize_seq(visitor)
-    }
-}
+    [f64;2] as f64x2,
+    [f64;4] as f64x4,
+    [f64;6] as f64x6,
+    [f64;8] as f64x8,
+    [f64;10] as f64x10,
+    [f64;12] as f64x12,
+    [f64;14] as f64x14,
+    [f64;16] as f64x16
+);
--- tests/main.rs
+++ tests/main.rs
@@ -1,7 +1,30 @@
+#![feature(concat_idents)]
 use llml_simd::float::single::*;
 use llml_simd::float::double::*;
 use core::ops::*;
 use rand::random;
+
+macro_rules! all_types {
+    () => {
+        [f32;2] as f32x2,
+        [f32;4] as f32x4,
+        [f32;6] as f32x6,
+        [f32;8] as f32x8,
+        [f32;10] as f32x10,
+        [f32;12] as f32x12,
+        [f32;14] as f32x14,
+        [f32;16] as f32x16,
+    
+        [f64;2] as f64x2,
+        [f64;4] as f64x4,
+        [f64;6] as f64x6,
+        [f64;8] as f64x8,
+        [f64;10] as f64x10,
+        [f64;12] as f64x12,
+        [f64;14] as f64x14,
+        [f64;16] as f64x16
+    };
+}
 
 macro_rules! test_other {
     ($($fun:ident $(as $name:ident)?),+) => {
@@ -164,17 +187,77 @@ macro_rules! test_index {
     }
 }
 
+macro_rules! test_rand {
+    ($([$ty:ident;$len:literal] as $target:ident),+) => {
+        $(
+            let array : $target = random();
+            assert_eq!(array, array);
+        )*
+    }
+}
+
+macro_rules! test_serde {
+    ($([$ty:ident;$len:literal] as $target:ident),+) => {
+        $(
+            let test : $target = random::<[$ty;$len]>().into();
+            let ser = serde_json::to_string(&test).unwrap();
+            let de = serde_json::from_str(&ser).unwrap();
+            assert_eq!(test, de);
+        )*
+    }
+}
+
 macro_rules! test_into {
     ($([$ty:ident;$len:literal] as $target:ident),+) => {
         $(
             let array : [$ty;$len] = random();
-            let simd = $target::from(array);
+            let into : $target = array.into();
+            let from : [$ty;$len] = into.into();
+            assert_eq!(array, from);
+        )*
+    }
+}
 
-            for i in 0..$len {
-                assert_eq!(array[i], simd[i], concat!("Assertion failed for '", stringify!($target), "'"));
+macro_rules! test_from {
+    ($([$ty:ident;$len:literal] as $target:ident),+) => {
+        $(
+            let scalar : $ty = random();
+            let vec : $target = scalar.into();
+
+            for i on 0..$len {
+                assert_eq!(vec[i], scalar);
             }
         )*
     }
+}
+
+macro_rules! test_various {
+    ($($name:ident),+) => {
+        $(
+            #[test]
+            pub fn $name () {
+                concat_idents!(test_, $name)(
+                    [f32;2] as f32x2,
+                    [f32;4] as f32x4,
+                    [f32;6] as f32x6,
+                    [f32;8] as f32x8,
+                    [f32;10] as f32x10,
+                    [f32;12] as f32x12,
+                    [f32;14] as f32x14,
+                    [f32;16] as f32x16,
+                
+                    [f64;2] as f64x2,
+                    [f64;4] as f64x4,
+                    [f64;6] as f64x6,
+                    [f64;8] as f64x8,
+                    [f64;10] as f64x10,
+                    [f64;12] as f64x12,
+                    [f64;14] as f64x14,
+                    [f64;16] as f64x16
+                );
+            }
+        )*
+    };
 }
 
 test_other!(
@@ -190,9 +273,14 @@ test_mappings!(
     neg, abs, sqrt
 );
 
+test_various!(
+    index
+);
+
+#[cfg(feature = "random")]
 #[test]
-pub fn index () {
-    test_index!(
+pub fn rnd () {
+    test_rand!(
         [f32;2] as f32x2,
         [f32;4] as f32x4,
         [f32;6] as f32x6,
@@ -213,7 +301,26 @@ pub fn index () {
     );
 }
 
+#[cfg(feature = "serialize")]
 #[test]
-pub fn into () {
-
+pub fn serialize () {
+    test_serde!(
+        [f32;2] as f32x2,
+        [f32;4] as f32x4,
+        [f32;6] as f32x6,
+        [f32;8] as f32x8,
+        [f32;10] as f32x10,
+        [f32;12] as f32x12,
+        [f32;14] as f32x14,
+        [f32;16] as f32x16,
+    
+        [f64;2] as f64x2,
+        [f64;4] as f64x4,
+        [f64;6] as f64x6,
+        [f64;8] as f64x8,
+        [f64;10] as f64x10,
+        [f64;12] as f64x12,
+        [f64;14] as f64x14,
+        [f64;16] as f64x16
+    );
 }
